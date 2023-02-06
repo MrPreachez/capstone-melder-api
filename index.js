00f92975edae8d407d67bfdc2a1368e3a1de54af
Damon  Chouinard <fengchoui@gmail.com>
2023-02-05 21:52:31 -0800

    routes file setup

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// const melderRoutes = require("./routes/inventory");
+const melderRoutes = require("./routes/routes");
 
-// app.use("/", (req, res, next) => {
-//   next();
-// });
+app.use("/", (req, res, next) => {
+  next();
+});
 
-// app.use("/warehouse", warehouseRoutes);
+app.use("/", melderRoutes);
 
 
 
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+router.route("/project").post(melderController.addProject);
+router.route("/project/:id").get(melderController.getProject);
+router.route("/project/:id").post(melderController.addInput);
+router.route("/submit_project/:id").post(melderController.submitProjectToAPI);
+router.route("/project/:id/results").get(melderController.getResult);
+
+module.exports = router;

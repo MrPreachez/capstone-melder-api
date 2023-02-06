2eb007d4293c203b2e83d7b880686310b2c871bb
Damon  Chouinard <fengchoui@gmail.com>
2023-02-05 23:47:15 -0800

    first post setup for addProject

--- controllers/melderController.js
+++ controllers/melderController.js
@@ -1,6 +1,26 @@
 const knex = require("knex")(require("../knexfile"));
 
-const
-module.exports = {
+const addProject = (req, res) => {
+  const { creator_name, project_name, question } = req.body;
+  knex("projects")
+  .insert({
+    creator_name,
+    project_name,
+    question
+  })
+  .then(() => {
+    knex.raw("SELECT LAST_INSERT_ID() as project_id")
+    .then(projectId => {
+        res.status(200).json({
+            project_id:projectId[0][0].project_id
+        });
+    })
+  });
+};
+// const getProject = (req, res) => {
 
+// }
+
+module.exports = {
+  addProject,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const melderController = require("../controllers/melderController");
 
 router.route("/project").post(melderController.addProject);
-router.route("/project/:id").get(melderController.getProject);
-router.route("/project/:id").post(melderController.addInput);
-router.route("/submit_project/:id").post(melderController.submitProjectToAPI);
-router.route("/project/:id/results").get(melderController.getResult);
+// router.route("/project/:id").get(melderController.getProject);
+// router.route("/project/:id").post(melderController.addInput);
+// router.route("/submit_project/:id").post(melderController.submitProjectToAPI);
+// router.route("/project/:id/results").get(melderController.getResult);
 
 module.exports = router;

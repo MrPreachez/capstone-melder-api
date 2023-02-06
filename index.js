9e2c0a0deda1eae477a35f9b343398ce0c578716
Damon  Chouinard <fengchoui@gmail.com>
2023-02-05 17:13:02 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8081;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const melderRoutes = require("./routes/inventory");
+
+app.use("/", (req, res, next) => {
+  next();
+});
+
+// app.use("/warehouse", warehouseRoutes);
+
+
+
+app.listen(PORT, function () {
+  console.log(`Listening on ${PORT}`);
+});

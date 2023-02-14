c22278ab3c939eb9fafaf5e5a8472ab54ffd4205
Damon  Chouinard <fengchoui@gmail.com>
2023-02-13 21:39:47 -0800

    .env var error update

--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,8 @@ require("dotenv").config();
 module.exports = {
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    database: process.env.DB_DBNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   },

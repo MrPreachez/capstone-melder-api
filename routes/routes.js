a8151315def40a0dbbe49738daee6c753f754797
Damon  Chouinard <fengchoui@gmail.com>
2023-02-14 11:21:37 -0800

    route fix

--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const fs = require("fs");
 const melderController = require("../controllers/melderController");
 
-router.route("project").post(melderController.addProject);
-router.route("project/:id").get(melderController.getProject);
-router.route("input").post(melderController.addResponse);
-router.route("project/:id").post(melderController.addResult);
-router.route("project/:id/result").get(melderController.getResult);
-router.route("project").get(melderController.getAllProjects);
-router.route("project/:id/all").get(melderController.getAllProjectData);
+router.route("/project").post(melderController.addProject);
+router.route("/project/:id").get(melderController.getProject);
+router.route("/input").post(melderController.addResponse);
+router.route("/project/:id").post(melderController.addResult);
+router.route("/project/:id/result").get(melderController.getResult);
+router.route("/project").get(melderController.getAllProjects);
+router.route("/project/:id/all").get(melderController.getAllProjectData);
 
-router.route("responses/:id").get(melderController.getResponses);
+router.route("/responses/:id").get(melderController.getResponses);
 
 module.exports = router;
